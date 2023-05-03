48bb586fe65c3737dbaf5e00b91e7e746be8f2d9
Siftal <m.khanmohamadzadeh@gmail.com>
2023-05-03 21:46:05 +0330

    feat: remove isDeployer

---
+++ contracts/IMuonNodeManagerV2.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+interface IMuonNodeManager {
+    struct Node {
+        uint64 id; // incremental ID
+        address nodeAddress; // will be used on the node
+        address stakerAddress;
+        string peerId; // p2p peer ID
+        bool active;
+        uint256 startTime;
+        uint256 endTime;
+        uint256 lastEditTime;
+    }
+
+    function addNode(
+        address _nodeAddress,
+        address _stakerAddress,
+        string calldata _peerId,
+        bool _active
+    ) external;
+
+    function deactiveNode(uint64 nodeId) external;
+
+    function stakerAddressInfo(address _addr) external view returns(
+        Node memory node
+    );
+}
